3c53f3b51279a2f4ebabc1c8c5992ebdd42a6a70
Dane Larsen <dane@larsendt.com>
2015-11-29 13:50:14 -0700

    main uses nonblocking rotate

--- driver/main.cpp
+++ driver/main.cpp
@@ -5,7 +5,8 @@
 #include "stepper.h"
 
 int main(int argc, char *argv[]) {
-    Stepper s(513, 0, 1, 2, 3);
+    Stepper s1(513, 0, 1, 2, 3);
+    Stepper s2(513, 4, 5, 6, 7);
     Magnetometer m;
 
     float cur_rot = 0;
@@ -15,7 +16,12 @@ int main(int argc, char *argv[]) {
         float rotation = (int)(heading - cur_rot);
         std::cout << "heading: " << heading << " rotation: " << rotation << std::endl;
         if(rotation != 0) {
-            s.rotate(rotation, 15);
+            s1.nonblockingRotate(rotation, 15);
+            s2.nonblockingRotate(rotation, 15);
+            while((!s1.doneRotating()) || (!s2.doneRotating())) {
+                s1.step();
+                s2.step();
+            }
             cur_rot = heading;
         }
         sleep(1);
