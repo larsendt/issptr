6336d569d760bbd36c1692a1fd6d25581c6c5be3
Dane Larsen <dane@larsendt.com>
2015-11-28 19:22:55 -0700

    stepper lib working

--- driver/stepper.cpp
+++ driver/stepper.cpp
@@ -1,9 +1,14 @@
 #include "stepper.h"
 #include <iostream>
+#include <time.h>
 
-Stepper::Stepper(int steps_per_rev) {
+Stepper::Stepper(int steps_per_rev, int a1_pin, int a2_pin, int b1_pin, int b2_pin) {
     _steps_per_rev = steps_per_rev;
     _gpio = WiringPiGPIO();
+    _a1 = a1_pin;
+    _a2 = a2_pin;
+    _b1 = b1_pin;
+    _b2 = b2_pin;
 }
 
 void Stepper::rotate(float deg, float rpm) {
@@ -14,17 +19,40 @@ void Stepper::rotate(float deg, float rpm) {
     float sec_per_step = sec_per_rev / _steps_per_rev;
     int ns_per_step = (int)(sec_per_step * 1e9);
 
-    if(deg > 0) {
-        _gpio.set(_dir_pin, 0);
+    for(int i = 0; i < steps/4; i++) {
+        if(deg > 0) {
+            setPins(1, 0, 1, 0);
+            sleep(ns_per_step);
+            setPins(0, 1, 1, 0);
+            sleep(ns_per_step);
+            setPins(0, 1, 0, 1);
+            sleep(ns_per_step);
+            setPins(1, 0, 0, 1);
+            sleep(ns_per_step);
+        }
+        else {
+            setPins(1, 0, 0, 1);
+            sleep(ns_per_step);
+            setPins(0, 1, 0, 1);
+            sleep(ns_per_step);
+            setPins(0, 1, 1, 0);
+            sleep(ns_per_step);
+            setPins(1, 0, 1, 0);
+            sleep(ns_per_step);
+        }
     }
-    else {
-        _gpio.set(_dir_pin, 1);
-    }
+}
 
-    int toggle = 1;
-    for(int i = 0; i < steps; i++) {
-        std::cout << "sleep " << ns_per_step << " ns; toggle " << toggle << std::endl;
-        _gpio.set(_step_pin, toggle);
-        //toggle = !toggle;
-    }
+void Stepper::setPins(int a1, int a2, int b1, int b2) {
+    _gpio.set(_a1, a1);
+    _gpio.set(_a2, a2);
+    _gpio.set(_b1, b1);
+    _gpio.set(_b2, b2);
+}
+
+void Stepper::sleep(int nsec) {
+    struct timespec ts;
+    ts.tv_sec = nsec / (int)1e9;
+    ts.tv_nsec = nsec - (ts.tv_sec * (int)1e9);
+    nanosleep(&ts, NULL);
 }
--- driver/stepper.h
+++ driver/stepper.h
@@ -4,13 +4,17 @@
 
 class Stepper {
     public:
-        Stepper(int steps_per_rev);
+        Stepper(int steps_per_rev, int a1_pin, int a2_pin, int b1_pin, int b2_pin);
         void rotate(float deg, float rpm);
 
     private:
+        void sleep(int nsec); 
+        void setPins(int a1, int a2, int b1, int b2);
+
         int _steps_per_rev;
         WiringPiGPIO _gpio;
-
-        const int _dir_pin = 0;
-        const int _step_pin = 0;
+        int _a1;
+        int _a2;
+        int _b1;
+        int _b2;
 };
