b8ee616bb90922635f2db2aaad475b0f3a1695e5
Dane Larsen <dane@larsendt.com>
2015-11-28 19:22:23 -0700

    add usage and argv input to main

--- driver/main.cpp
+++ driver/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
-
+#include <stdlib.h>
 #include "stepper.h"
 
 int main(int argc, char *argv[]) {
-    Stepper s(513);
-    s.rotate(60, 200);
+    if(argc < 3) { 
+        std::cout << "usage: " << argv[0] << " <deg> <rpm>" << std::endl;
+        return 1;
+    }
+
+    Stepper s(513, 0, 1, 2, 3);
+    s.rotate(atoi(argv[1]), atoi(argv[2]));
     return 0;
 }
