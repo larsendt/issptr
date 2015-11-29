1400cac9c6605d5d9d876eb60bffdbfd23a2a507
Dane Larsen <dane@larsendt.com>
2015-11-29 13:50:04 -0700

    add nonblocking rotate capability to stepper

--- driver/stepper.cpp
+++ driver/stepper.cpp
@@ -10,11 +10,43 @@ Stepper::Stepper(int steps_per_rev, int a1_pin, int a2_pin, int b1_pin, int b2_p
     _b1 = b1_pin;
     _b2 = b2_pin;
 
+    _rotating = false;
+
     wiringPiSetup();
     pinMode(_a1, OUTPUT);
     pinMode(_a2, OUTPUT);
     pinMode(_b1, OUTPUT);
     pinMode(_b2, OUTPUT);
+}
+
+void Stepper::nonblockingRotate(float deg, float rpm) {
+    _rotating = true;
+
+    _total_steps = (int)((deg / 360.0) * _steps_per_rev);
+    _total_steps = _total_steps > 0 ? _total_steps : -_total_steps;
+    _steps_done = 0;
+
+    float sec_per_rev = 60.0 / rpm;
+    float sec_per_step = sec_per_rev / _steps_per_rev;
+    _ns_per_step = (int)(sec_per_step * 1e9);
+    _direction = deg >= 0;
+}
+
+void Stepper::step() {
+    if(!_rotating) {
+        return;
+    }
+    else if(_steps_done == _total_steps) {
+        _rotating = false;
+        return;
+    }
+
+    step(_steps_done, _direction, _ns_per_step);
+    _steps_done += 1;
+}
+
+bool Stepper::doneRotating() {
+    return !_rotating;
 }
 
 void Stepper::rotate(float deg, float rpm) {
@@ -61,4 +93,44 @@ void Stepper::sleep(int nsec) {
     ts.tv_sec = nsec / (int)1e9;
     ts.tv_nsec = nsec - (ts.tv_sec * (int)1e9);
     nanosleep(&ts, NULL);
+}
+
+void Stepper::step(int step_num, int direction, int ns_per_step) {
+    if(direction > 0) {
+        switch(step_num % 4) {
+            case 0:
+                setPins(1, 0, 1, 0);
+                break;
+            case 1:
+                setPins(0, 1, 1, 0);
+                break;
+            case 2:
+                setPins(0, 1, 0, 1);
+                break;
+            case 3:
+                setPins(1, 0, 0, 1);
+                break;
+            default:
+                std::cerr << "Default case in step pattern!!! THIS SHOULD NEVER HAPPEN" << std::endl;
+        }
+    }
+    else {
+        switch(step_num % 4) {
+            case 0:
+                setPins(1, 0, 0, 1);
+                break;
+            case 1:
+                setPins(0, 1, 0, 1);
+                break;
+            case 2:
+                setPins(0, 1, 1, 0);
+                break;
+            case 3:
+                setPins(1, 0, 1, 0);
+                break;
+            default:
+                std::cerr << "Default case in step pattern!!! THIS SHOULD NEVER HAPPEN" << std::endl;
+        }
+    }
+    sleep(ns_per_step);
 }
--- driver/stepper.h
+++ driver/stepper.h
@@ -4,14 +4,24 @@ class Stepper {
     public:
         Stepper(int steps_per_rev, int a1_pin, int a2_pin, int b1_pin, int b2_pin);
         void rotate(float deg, float rpm);
+        void nonblockingRotate(float deg, float rpm);
+        void step();
+        bool doneRotating();
 
     private:
         void sleep(int nsec); 
         void setPins(int a1, int a2, int b1, int b2);
+        void step(int step_num, int direction, int ns_per_step);
 
         int _steps_per_rev;
         int _a1;
         int _a2;
         int _b1;
         int _b2;
+
+        bool _rotating;
+        int _total_steps;
+        int _steps_done;
+        int _ns_per_step;
+        int _direction;
 };
